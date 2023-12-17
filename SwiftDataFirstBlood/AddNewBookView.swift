0539bedc4303c5b3b436986d2ba2b741f43ec7ba
Jacek Kosinski <kashiash@gmail.com>
2023-12-17 23:55:08 +0100

    AddNewBookView

---
+++ SwiftDataFirstBlood/AddNewBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewBookView.swift
+//  SwiftDataFirstBlood
+//
+//  Created by Jacek Kosinski U on 17/12/2023.
+//
+
+import SwiftUI
+
+struct AddNewBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewBookView()
+}
---
+++ SwiftDataFirstBlood/Model/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  SwiftDataFirstBlood
+//
+//  Created by Jacek Kosinski U on 17/12/2023.
+//
+
+import Foundation
