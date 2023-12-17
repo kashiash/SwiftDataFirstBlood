dc0073fe63a6aebcf4e644971ff108a4920fd65b
Jacek Kosinski <kashiash@gmail.com>
2023-12-17 23:07:53 +0100

    Initial Commit

---
+++ SwiftDataFirstBlood/SwiftDataFirstBloodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataFirstBloodApp.swift
+//  SwiftDataFirstBlood
+//
+//  Created by Jacek Kosinski U on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataFirstBloodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
